f627ed3a4474a5485709f0fca9bf85337279849b
Engity <ngtoan1107@gmail.com>
2023-08-06 14:44:00 -0700

    Update Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './style/App.css';
 import Login from './features/auth/Login';
 import HomePage from './features/auth/HomePage';
 import Problem from './features/problems/ProblemsList';
+import useTitle from './hooks/useTitle';
 
 
 function App() {
@@ -15,14 +16,16 @@ function App() {
   //     }
   //   </div>
   // );
+  useTitle('Automatic Grading Contest')
   return (
     <Routes>
       <Route exact path="/" element={<Layout/>}>
         <Route index element = {<Login/>} />
         <Route path="home" element={<HomePage/>}/>
-        
+      
       </Route>
       <Route exact path="/problem" element={<Problem/>} />
+      <Route path="*" component={HomePage} />  Always redirected to homepage if requested a missing resources
     </Routes>
   )
 }
