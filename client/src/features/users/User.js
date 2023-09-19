1ab9aef4ed3a98036a6d3a7a2fc5b9d5552e3953
Aaron Tran <100805011+aaront99@users.noreply.github.com>
2023-09-19 15:41:42 -0700

    Finalized design and implementations for users

--- client/src/features/users/User.js
+++ client/src/features/users/User.js
@@ -1,55 +1,57 @@
+
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectUserById } from './usersApiSlice'
 import { useDeleteUserMutation } from './usersApiSlice'
 
 
-const User = ({ userId }) => {
-   
+const User = ({ userId, isChecked = false, setIsChecked }) => {
+
     const user = useSelector(state => selectUserById(state, userId))
+    
+   
 
     const navigate = useNavigate()
-
-    const userRolesString = user.roles.toString().toLowerCase().replaceAll(',', ', ');
     
+    
+    const userRolesString = user.roles.toString().toLowerCase().replaceAll(',', ', ');
+
     // Check if userRolesString contains "contestant"
     const hasContestantRole = userRolesString.includes('contestant');
 
-    const [deleteUser, {
-        isSuccess: isDelSuccess,
-        isError: isDelError,
-        error: deleerror
-    }] = useDeleteUserMutation()
+    const [deleteUser] = useDeleteUserMutation()
 
     const handleDelete = async () => {
         await deleteUser({ id: userId })
     }
 
-    const handleEdit = () => {
+    const handleEdit = () => navigate(`/users/${userId}`)
 
-    };
+    const handleCheckBox = () => {  
+        setIsChecked(!isChecked)
+    }
 
-   
-   
     if (hasContestantRole) {
-        const handleEdit = () => navigate(`/users/${userId}`)
-
-        const cellStatus = user.active ? '' : 'table__cell--inactive'
-
         return (
-            <div className='user-card'>
-                
-                <div className='user-info'>
-                    
-                    <p className='username'>{user.username}</p>
-                    <p className='roles'>{userRolesString}</p>
+
+
+            <div className='user'>
+                <div className='user-checkbox'>
+                    <input type='checkbox' checked={isChecked} onChange={handleCheckBox}></input>
                 </div>
-                <div className='user-action'>
-                    <button onClick={handleEdit}><i class="bi bi-pencil-fill"></i></button>
-                    <button onClick={handleDelete}><i class="bi bi-trash3"></i></button>
+                <div className={`user-card ${isChecked ? 'highlight' : ''}`}>
+                    <div className='user-info'>
+                        <p className='username'>{user.username}</p>
+                        <p className='roles'>{userRolesString}</p>
+                    </div>
+                    <div className='user-action'>
+                        <button className='edit-button' onClick={handleEdit}><i class="bi bi-pencil-fill"></i></button>
+                        <button className='delete-button' onClick={handleDelete}><i class="bi bi-trash3"></i></button>
+                    </div>
+
                 </div>
-               
             </div>
+
         )
     } else {
         return null;
