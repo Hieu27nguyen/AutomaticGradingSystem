0e0bfe953a354371a0aaa572fcd7acf99fe1467f
Engity <ngtoan1107@gmail.com>
2023-11-28 00:15:01 -0800

    Added create submissions
    Connect with judge0 to judge program.
    Store results in DB

--- server/routes/submissionRoutes.js
+++ server/routes/submissionRoutes.js
@@ -5,10 +5,18 @@ const submissionControllers = require('../controllers/submissionsController')
 router.route('/')
     .get(submissionControllers.getAllSubmissions)
     .post(submissionControllers.createSubmission)
-    .patch(submissionControllers.updateSubmission)
-    .delete(submissionControllers.deleteSubmission)
 
 // Define a new route to get supported languages
 router.route('/languages').get(submissionControllers.getSupportedLanguage);
+
+//Get submission records by username
+// Full URI: http://localhost:port/submission/username/:username
+// Required field in request body:
+router.route('/username/:username').get(submissionControllers.getSubmissionByUsername);
+
+//Get submission records by id
+// Full URI: http://localhost:port/submission/id/:id
+// Required field in request body:
+router.route('/id/:id').get(submissionControllers.getSubmissionByID);
 
 module.exports = router
