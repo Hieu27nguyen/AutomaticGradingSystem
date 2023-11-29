e1ee22a32d0d9b0d8079ce63ecbd08cc437619be
Engity <ngtoan1107@gmail.com>
2023-11-29 14:27:07 -0800

    Update Scoreboard.js

--- server/models/Scoreboard.js
+++ server/models/Scoreboard.js
@@ -3,22 +3,38 @@ const mongoose = require('mongoose')
 const scoreboardSchema = new mongoose.Schema({
     username: {//username of the contestant
         type: String,
+        ref: 'User',
         required: true
     },
     problemSolved: {
-
+        type: Number,
+        required: true
     },
-    totalScore:{//Total score achieved
-
+    totalScore: {//Total score achieved
+        type: Number,
+        required: true
     },
 
     problemStatistic: [{//Containing detail submission records
-        problemID: String,
-        ref: 'Problem',
-        required: true,
+        problemID: {
+            type: Number,
+            ref: 'Problem',
+            required: true,
+        },
+       
         submissionRecord: { //Keep track of submission attempts, penalty and score for each problem
-
-
+            attempts: {
+                type: Number,
+                required: true,
+            },
+            penalty: {
+                type: Number,
+                required: true,
+            },
+            score: {
+                type: Number,
+                required: true,
+            },
         }
     }],
 
