6a74c43028ed6f9bb7fa761bcd8c0155bf156259
Engity <ngtoan1107@gmail.com>
2023-12-08 02:36:11 -0800

    Update problemPlayground.mongodb.js

--- server/playground/problemPlayground.mongodb.js
+++ server/playground/problemPlayground.mongodb.js
@@ -40,49 +40,110 @@ let importData = async (data) => {
 const problemsData = [
 
     //Test 00
-    //Testing duplicate problem id
+    //Solution with Python (3.8.1), beware of indent, Added the D
+    //text = input()
+    //print(text+'d')
     {
-        _id: ObjectID("Prob0"),
-        name: "Problem00",
-        description: "yessir",
-        judgeProgram: "",
-        test: [{ input: "abc", output: "dfc" }],
-    },
-    {
-        _id: ObjectID("Prob1"),
-        name: "Problem01",
-        description: "nosir",
+        // _id: ObjectID("Prob0"),
+        name: "Added the D",
+        description: "Added only 1 letter 'd' to the end of every input",
         judgeProgram: "",
         test: [
-            { input: "abc", output: "abc" },
-            { input: "abcd", output: "dfcg" }
+            { input: "abc", output: "abcd" },
+            { input: "ddd", output: "dddd" },
+            { input: "eee", output: "eeed" },
         ],
+        memLimit: 128000,
+        timeLimit: 2,
+        penaltyMinute: 20,
     },
+
+    //Solution for problem 1 with Java Say No
+    // import java.util.*;
+    // import java.lang.*;
+    // import java.io.*;
+
+    // class Ideone
+    // {
+    // 	public static void main (String[] args) throws java.lang.Exception
+    // 	{
+    // 		Scanner input = new Scanner(System.in);
+    // 		if (input.next().toLowerCase().equals("yes")){
+    // 			System.out.print("Yes");
+    // 		}
+    // 		else{
+    // 			System.out.print("No");
+    // 		}
+    // 	}
+    // }
     {
-        _id: ObjectID("Prob2"),
-        name: "Problem02",
-        description: "asdsad",
-        judgeProgram: `
-            const judgingFunction = (contestantOutput, problemInput = "") => {
-                let num = parseInt(contestantOutput);
-                let res = false;
-                try {
-                    if (num % 2 === 0) {
-                        return true;
-                    }
-                } catch (error) {
-                    // If there is an error cannot parse the output
-                    return false;
-                }
-                return res;
-            };
-        `
-        ,
+        // _id: ObjectID("Prob1"),
+        name: "Say No",
+        description: "Print \"No\", except if the input is yes then say \"Yes\"",
+        judgeProgram: "",
         test: [
-            { input: "2", output: "" },
-            { input: "4", output: "" }
+            { input: "abc", output: "No" },
+            { input: "Yes", output: "Yes" }
         ],
-    }
+        memLimit: 128000,
+        timeLimit: 2,
+        penaltyMinute: 20,
+    },
+
+    // Solution for Problem 2 Add 2 to input
+    // import java.util.*;
+    // import java.lang.*;
+    // import java.io.*;
+
+    // class Ideone
+    // {
+    // 	public static void main (String[] args) throws java.lang.Exception
+    // 	{
+    // 		Scanner input = new Scanner(System.in);
+    // 		Integer t = input.nextInt();
+    // 		System.out.print(t + 2);
+    // 	}
+    // }
+    {
+        // _id: ObjectID("Prob2"),
+        name: "Add 2 to input",
+        description: "Added 2 to any number in the input",
+        judgeProgram: "",
+        test: [
+            { input: "2", output: "4" },
+            { input: "6", output: "8" },
+            { input: "12319", output: "12321" }
+        ],
+        memLimit: 128000,
+        timeLimit: 2,
+        penaltyMinute: 20,
+    },
+
+    // {
+    //     _id: ObjectID("Prob2"),
+    //     name: "Problem02",
+    //     description: "asdsad",
+    //     judgeProgram: `
+    //         const judgingFunction = (contestantOutput, problemInput = "") => {
+    //             let num = parseInt(contestantOutput);
+    //             let res = false;
+    //             try {
+    //                 if (num % 2 === 0) {
+    //                     return true;
+    //                 }
+    //             } catch (error) {
+    //                 // If there is an error cannot parse the output
+    //                 return false;
+    //             }
+    //             return res;
+    //         };
+    //     `
+    //     ,
+    //     test: [
+    //         { input: "2", output: "" },
+    //         { input: "4", output: "" }
+    //     ],
+    // }
 ];
 
 // Import problems data
