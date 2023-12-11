142f171f958f6425543514e861622df277d5f44e
Engity <ngtoan1107@gmail.com>
2023-12-10 22:05:40 -0800

    Update root.js

--- server/routes/root.js
+++ server/routes/root.js
@@ -5,9 +5,42 @@ const path = require('path')
  * @swagger
  * tags:
  *  - name: Authentication
- *    description: IMPORTANT`:` Execute this API before testing others. The username and password are already defined. After successfully login, copy the access token and paste it in Authorize button before using other APIs. 
+ *    description: |
+ *     ##### IMPORTANT: Handle security with basic functionality like login, refresh and logout.
+ *     ##### Run the User Mongo playground then execute this API before trying others with the pre-defined username and password. 
+ *     ##### After successfully login, copy the access token and paste it in Authorize button before using other APIs. 
+ * 
  *  - name: Announcements
- *    description: Annoucement Route
+ *    description: |
+ *     ##### Allow Judges to communicate with all contestants during a contest.
+ * 
+ *  - name: Users
+ *    description: |
+ *     ##### Allow judges manage contestant.
+ *     ##### Store users' credential and help with authentication.
+ * 
+ *  - name: Competitions
+ *    description: |
+ *     ##### Help judges configure a competition like start time and duration.
+ *     ##### Contestants can also get the detail about competition here.
+ * 
+ *  - name: Problems
+ *    description: |
+ *     ##### Manage problems for the competition.
+ * 
+ *  - name: Scoreboard
+ *    description: |
+ *     ##### View the scoreboard of the current contest.
+ * 
+ *  - name: Submissions
+ *    description: |
+ *     ##### Help judges view all submissions
+ *     ##### Contestant can create and view their own submissions
+ * 
+ *  - name: Translations
+ *    description: |
+ *     ##### Request translation for a particular word or phrase
+ *     ##### Service provided by Google Cloud Translation API.
  */
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
